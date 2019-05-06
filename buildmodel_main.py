673f0f19040f077e7788e0d04e8b8ccaf65f60e6
Albert Liu <v36239@gmail.com>
2019-05-06 13:57:09 +0800

    build1

--- buildmodel_main.py
+++ buildmodel_main.py
@@ -35,13 +35,13 @@ Set_Random_Forest:Double array * 4(X training data, X testing data, y training d
 '''
 
 
-def Read_from_csv(path):
+def read_from_csv(path):
     test_array = pd.read_csv(path)
     print(test_array.head)
     return test_array
 
 
-def Set_Decision_Tree(X1_train, X1_test, y1_train, y1_test):
+def set_Decision_Tree(X1_train, X1_test, y1_train, y1_test):
     decision_Tree_Model = DecisionTreeClassifier()
     decision_Tree_Model.fit(X1_train, y1_train)
     decision_Tree_Model_result = decision_Tree_Model.predict(X1_test)
@@ -52,7 +52,7 @@ def Set_Decision_Tree(X1_train, X1_test, y1_train, y1_test):
     return decision_Tree_Model
 
 
-def Set_Random_Forest(X1_train, X1_test, y1_train, y1_test):
+def set_Random_Forest(X1_train, X1_test, y1_train, y1_test):
     random_forest_Model = RandomForestClassifier(criterion= 'entropy', n_estimators= 50, random_state= 3, n_jobs = 2)
     random_forest_Model.fit(X1_train, y1_train)
     random_forest_Model_result = random_forest_Model.predict(X1_test)
@@ -69,10 +69,10 @@ def main():
     try:
 
         # data input
-        acce_file_path = 'D:/Year_PROJ/watch_predict-1.0/input_data/Accelerometer_Merge_Log.csv' # you need to add acceleration data's path here (csv file)
-        gyro_file_path = 'D:/Year_PROJ/watch_predict-1.0/input_data/Gyro_Merge_Log.csv' # you need to add Gyroscope data's path here (csv file)
-        acce_res = Read_from_csv(acce_file_path)
-        gyro_res = Read_from_csv(gyro_file_path)
+        acce_file_path = 'D:/WORK4/watch_predict-1.0/input_data/Accelerometer_Merge_Log.csv' # you need to add acceleration data's path here (csv file)
+        gyro_file_path = 'D:/WORK4/watch_predict-1.0/input_data/Gyro_Merge_Log.csv' # you need to add Gyroscope data's path here (csv file)
+        acce_res = read_from_csv(acce_file_path)
+        gyro_res = read_from_csv(gyro_file_path)
 
         # new dataset
         acce_X = acce_res[['acceleration_x', 'acceleration_y', 'acceleration_z']]
@@ -99,8 +99,8 @@ def main():
         gyro_X_train, gyro_X_test, gyro_y_train, gyro_y_test = train_test_split(gyro_X, gyro_y, test_size=0.2, random_state=0)
 
         # test data(Decision Tree / Random forest)
-        model_For_acce = Set_Random_Forest(acce_X_train, acce_X_test, acce_y_train, acce_y_test)
-        model_For_gyro = Set_Random_Forest(gyro_X_train, gyro_X_test, gyro_y_train, gyro_y_test)
+        model_For_acce = set_Random_Forest(acce_X_train, acce_X_test, acce_y_train, acce_y_test)
+        model_For_gyro = set_Random_Forest(gyro_X_train, gyro_X_test, gyro_y_train, gyro_y_test)
 
     except Exception as e:
 
--- predict_main.py
+++ predict_main.py
@@ -13,13 +13,13 @@ Parameters
 --------
 
 
-Read_from_csv: string, optional(file's path, only for .csv file)
+read_from_csv: string, optional(file's path, only for .csv file)
     Read the csv file and put them into dataframe
 
-Read_from_pickle: string, optional(file's path, only for .pkl file)
+read_from_pickle: string, optional(file's path, only for .pkl file)
     Read the model file and put them into process
 
-Predict_result: model(pkl), data(Double array)
+rredict_result: model(pkl), data(Double array)
 	use input data and model to predict result and output
 
 output_csv: data(array), string(path)
@@ -30,18 +30,18 @@ output_csv: data(array), string(path)
 
 
 
-def Read_from_csv(path):
+def read_from_csv(path):
     test_array = pd.read_csv(path)
     print(test_array.head)
     return test_array
 
 
-def Read_from_pickle(path):
+def read_from_pickle(path):
 	model = joblib.load(path)
 	return model
 
 
-def Predict_result(model, data):
+def predict_result(model, data):
 	result = model.predict(data)
 	return result
 
@@ -49,6 +49,7 @@ def Predict_result(model, data):
 def output_csv(data_predict, file_path):
 	data_adjust = pd.DataFrame(data_predict)
 	data_adjust.to_csv(file_path, index=False)
+	return None
 
 
 # main
@@ -71,28 +72,28 @@ def main():
 
 	'''
 	# input data
-	acce_test_path = 'D:/Year_PROJ/watch_predict-1.0/input_data/Accelerometer_Merge_Log.csv'		# input your test data path
-	gyro_test_path = 'D:/Year_PROJ/watch_predict-1.0/input_data/Gyro_Merge_Log.csv'		# input your test data path
+	acce_test_path = 'D:/WORK4/watch_predict-1.0/input_data/Accelerometer_Merge_Log.csv'		# input your test data path
+	gyro_test_path = 'D:/WORK4/watch_predict-1.0/input_data/Gyro_Merge_Log.csv'		# input your test data path
 
 	# input model
-	acce_model_path = 'D:/Year_PROJ/watch_predict-1.0/saved_model/md1_acce.pkl'		# input your model path
-	gyro_model_path = 'D:/Year_PROJ/watch_predict-1.0/saved_model/md2_gyro.pkl'		# input your model path
+	acce_model_path = 'D:/WORK4/watch_predict-1.0/saved_model/md1_acce.pkl'		# input your model path
+	gyro_model_path = 'D:/WORK4/watch_predict-1.0/saved_model/md2_gyro.pkl'		# input your model path
 
 	# output data
 	acce_output_path = 'output_data/acce_result.csv'
 	gyro_output_path = 'output_data/gyro_result.csv'
 
 	# read file & choose attribute
-	acce_temp = Read_from_csv(acce_test_path)
-	gyro_temp = Read_from_csv(gyro_test_path)
+	acce_temp = read_from_csv(acce_test_path)
+	gyro_temp = read_from_csv(gyro_test_path)
 	acce_data = acce_temp[['acceleration_x', 'acceleration_y', 'acceleration_z']]	# input attribute you want to predict(acceleration data)
 	gyro_data = gyro_temp[['gyro_x', 'gyro_y', 'gyro_z']]	# input attribute you want to predict(gyro data)
-	acce_model = Read_from_pickle(acce_model_path)
-	gyro_model = Read_from_pickle(gyro_model_path)
+	acce_model = read_from_pickle(acce_model_path)
+	gyro_model = read_from_pickle(gyro_model_path)
 
 	# predict
-	acce_result = Predict_result(acce_model, acce_data)
-	gyro_result = Predict_result(gyro_model, gyro_data)
+	acce_result = predict_result(acce_model, acce_data)
+	gyro_result = predict_result(gyro_model, gyro_data)
 	print(acce_result, '\n', gyro_result)
 
 	# output
