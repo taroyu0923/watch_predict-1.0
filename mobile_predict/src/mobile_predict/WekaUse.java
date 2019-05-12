a00f105e246159ade0dc93a9344f258e19091cf4
Albert Liu <v36239@gmail.com>
2019-05-12 20:46:09 +0800

    build for method

--- mobile_predict/src/mobile_predict/WekaUse.java
+++ mobile_predict/src/mobile_predict/WekaUse.java
@@ -1,65 +1,28 @@
 package mobile_predict;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.ObjectInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
-import weka.core.Attribute;
-import weka.classifiers.Classifier;
 import weka.classifiers.trees.RandomForest;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
 import weka.core.Instance;
-import weka.core.FastVector;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
-import weka.core.converters.ArffLoader;
-import weka.core.converters.ConverterUtils;
-import weka.core.converters.ConverterUtils.DataSource;
 
 
 
 public class WekaUse {
 
-	
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
+	public static int getResult(String path, Instance inputNum) throws FileNotFoundException, Exception 
+	{
+		int result = 0;
+		Instance dataRaw = inputNum;
+		RandomForest rf = (RandomForest) SerializationHelper.read(new FileInputStream(path));
 		
-		String path = "Accle_model.model";
-        RandomForest treeClassifier = (RandomForest) SerializationHelper.read(new FileInputStream("Accle_model.model"));
-		
-		/*DataSource source = new DataSource("Accelerometer_Merge_Log_test.csv");
-		Instances testData = source.getDataSet();
-		testData.setClassIndex(testData.numAttributes() -1);
-		
-		
-		
-		for(int i = 0; i < testData.numInstances(); i++) 
-		{
-			double testNum = testData.instance(i).classValue();
-			String output = testData.classAttribute().value((int) testNum);
-			
-			Instance newIns = testData.instance(i);
-			double pred = treeClassifier.classifyInstance(newIns);
-			String predString = testData.classAttribute().value((int) pred);
-			System.out.println(predString);
-		}*/
-		
-		
-		ConverterUtils.DataSource source2 = new ConverterUtils.DataSource("Accelerometer_Merge_Log_test.csv");
-        Instances test = source2.getDataSet();
-        // setting class attribute if the data format does not provide this information
-        // For example, the XRFF format saves the class attribute information as well
-        if (test.classIndex() == -1)
-            test.setClassIndex(test.numAttributes() - 1);
-
+		double label = rf.classifyInstance(dataRaw);
         
-        
-        int result = 0;
-        
-        
-        double label = treeClassifier.classifyInstance(test.instance(0));
-        
-        //test.instance(0).setClassValue(label);
-        //System.out.println(test.instance(0).value(3));
         System.out.println(label);
         if(label <= 0.5) 
         {
@@ -70,6 +33,46 @@ public class WekaUse {
         	result = 1;
         }
         System.out.println(result);
+		
+		return result;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		int resultNum;
+		String path = "Accle_model.model";
+		
+		
+		/*
+		 * input hint!
+		 * 1. Announce a ArrayList<Attribute> object
+		 * 2. Add your attribute
+		 * 3. Announce a empty Instances
+		 * 4. Set your attributes
+		 * 5. Create a DenseInstance & add it into your empty Instances
+		 * 6. Call function
+		 * */
+		//Announce ArrayList<Attribute>
+		ArrayList<Attribute> adjust = new ArrayList<Attribute>();
+		adjust.add(new Attribute("accr_x"));
+		adjust.add(new Attribute("accr_y"));
+		adjust.add(new Attribute("accr_z"));
+		
+		//Announce instances
+		Instances data = new Instances("TestInstances", adjust, 0);
+		data.setClassIndex(data.numAttributes() - 1);
+
+		//Set attributes
+		double testValue[] = {-10.27, -6.35, -0.225};
+		
+		//Create a DenseInstance
+		Instance testInstance = new DenseInstance(1.0, testValue);
+		testInstance.setDataset(data);
+		
+		//Call function
+		resultNum = getResult(path, testInstance);
+		
 
 	}
 
